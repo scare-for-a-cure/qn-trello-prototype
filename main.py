be70fdae02a0b6d9f48a9e2659b09daf8b8b964b
Chad R. Miller <chad.r.miller@gmail.com>
2025-12-19 21:57:11 +0000

    Some trello pre-work

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# This is a sample Python script.
+from trello import TrelloApi
+from secrets import TRELLO_API_KEY,TRELLO_BOARD_ID,TRELLO_SECRET,TRELLO_TOKEN
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def main():
@@ -9,8 +8,12 @@ def main():
     # Trello API
     # Various Google APIs for working with Google Docs.
     print(f'Hi, Chad')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+    trello = TrelloApi(TRELLO_API_KEY)
+    trello.set_token(TRELLO_TOKEN)
+    trello.boards.get(TRELLO_BOARD_ID)
+    print(trello)
+    card = trello.cards.get("pyhDPxft")
+    print(card)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
